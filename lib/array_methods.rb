bb66f4467ea0ccc4dbf54964a9b05c7b7f458d04
jules blanc <jules.blanc@unccub.com>
2019-10-03 12:35:39 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,14 @@
 def find_element_index(array, value_to_find)
-  for array {
+  counter = 0
+  while counter < array.length {
+    if array[counter] == value_to_find
+      return counter + 1 
+    end
+  }
 end
 
 def find_max_value(array)
-  # Add your solution here
+  return array.sort.uniq.last
 end
 
 def find_min_value(array)
